2db8cb4b8ac8d56288b4dbc79d32e524b66dc2bb
leguter <kolalevcenko80@gmail.com>
2024-06-24 22:19:50 -1200

    1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,20 +1,20 @@
-export function searchImg(value) {
-    const baseUrl = 'https://pixabay.com';
-    const endPoint = '/api/'
-    const options = new URLSearchParams({
-        key: '44411802-6a5316a8fc33c8235d915a6fe',
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        q:`${value}`,
-    });
-    const url = `${baseUrl}${endPoint}?${options}`;
-    return fetch(url).then(data => {
-        if (!data.ok) {
-        throw new Error(data.status);
-        } else {
-            return data.json();
-    }
-        
-     })
+import axios from "axios";
+export async function searchImg(value) {
+    axios.defaults.baseURL = 'https://pixabay.com';
+
+  const params = {
+    key: '44349742-ecc8a7b60aea5585f0c207813',
+    q: value,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+
+  try {
+    const response = await axios.get('/api/', { params });
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
 };
+searchImg('dog')
